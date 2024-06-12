56dbe83d72236f5425e2c291bb0dc0c826d1e5e6
Sonam Tenzin <02230300.cst@rub.edu.bt>
2024-06-12 17:14:33 +0600

    schema and profile ep added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client';
 
 const app = new Hono()
+const prisma = new PrismaClient();
 
-app.get('/', (c) => {
+app.get('/auth', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/profile/:username', (c) => {
+  const { username } = c.req.param();
+  const profile = prisma.profile.findUnique({
+    where: {
+      username,
+      following: {},
+      followers: {},
+      posts: {
+        include: {
+          likes: {
+            include: {
+              user: true
+            }
+          },
+          comments: {
+            include: {
+              user: true
+            }
+          }
+        }
+      }
+    }
+  });
+  return c.json(profile);
+})
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
